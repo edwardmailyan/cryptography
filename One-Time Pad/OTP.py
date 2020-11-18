bbf5204fee3ea90d33835f7d9125ffa1f156f237
Edward <may-ed@mail.ru>
2020-11-18 14:45:57 +0300

    Добавлено шифрование "одноразовый блокнот"

---
+++ One-Time Pad/OTP.py
@@ -0,0 +1,60 @@
+from random import randint
+
+alphabet = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж',
+            'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О',
+            'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц',
+            'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю',
+            'Я', '0', '1', '2', '3', '4', '5', '6',
+            '7', '8', '9', '.', ',', '!', '?', ' ']
+
+text = input('Введите текст: ')
+
+key = str(randint(2, len(text)))
+
+
+# Комбинация индексов кюча и сообщения
+def combine(msg, key):
+    idx = []
+    k = 0
+    for char in msg:
+        if k < len(key):
+            idx.append([char, int(key[k])])
+            k += 1
+        else:
+            k = 0
+            idx.append([char, int(key[k])])
+            k += 1
+    return idx
+
+
+def code_to_text(code):
+    text = ''
+    for char in code:
+        text += alphabet[int(char)]
+    return text
+
+
+def encode(msg, key):
+    msg = msg.upper()
+    letter_key = combine(msg, key)
+
+    encoded = []
+    for letter, num in letter_key:
+        encoded.append((alphabet.index(letter) + 1 + num) % len(alphabet))
+    return code_to_text(encoded)
+
+
+def decode(msg, key):
+    msg = msg.upper()
+    letter_key = combine(msg, key)
+
+    decoded = []
+    for letter, num in letter_key:
+        decoded.append((alphabet.index(letter) - 1 - num) % len(alphabet))
+    return code_to_text(decoded)
+
+
+a = encode(text, key)
+
+print(a)
+print(decode(a, key))
